6cd25f57e677bf53f08bcba6aa12a6cbdbae2aa1
Dark Tron <darktron1337@gmail.com>
2021-04-28 13:57:04 +0200

    record with puppeteer-video-recorder

--- example.js
+++ example.js
@@ -1,15 +1,23 @@
 const puppeteer = require('puppeteer');
+const PuppeteerVideoRecorder = require('puppeteer-video-recorder');
 
 const BBB_ROOM = 'sus-c9t-jhz';
+const recorder = new PuppeteerVideoRecorder();
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ['--lang=en-EN,en']
+    args: [
+      '--lang=en-EN,en',
+    ],
+    ignoreDefaultArgs: [
+      '--mute-audio'
+    ],
+    defaultViewport: {width: 1280, height: 720}
   });
 
   const page = await browser.newPage();
-  await page.setViewport({width: 1920, height: 1080});
+  await recorder.init(page, 'recordings');
 
   // go to bbb
   await page.goto('https://webconf.tu-bs.de/' + BBB_ROOM);
@@ -18,10 +26,13 @@ const BBB_ROOM = 'sus-c9t-jhz';
   await (await page.$('#_' + BBB_ROOM + '_join_name')).type('Nagrywarka');
   await (await page.$('button[type=submit]')).click();
 
-  // cookies
+  // accept cookies
   await page.waitForNavigation({
     waitUntil: 'networkidle0',
-  });  
+  });
+  await page.waitForSelector('input[type=submit]', {
+      timeout: 1000 * 60 * 60 // one hour
+  });
   await (await page.$('input[type=submit]')).click();
 
   // join audio session
@@ -32,9 +43,14 @@ const BBB_ROOM = 'sus-c9t-jhz';
   await (await page.$('button[aria-label="Listen only"]')).click();
 
   // start recording
-  await page.screenshot({ path: 'example.png' });
+  await recorder.start({
+      quality: 70
+  });
+  //page.waitForTimeout(5000);
+  const input = await page.$('#message-input');
+  await input.type('in english i guess', { delay: 200 });
 
   // end recording
-
+  await recorder.stop();
   await browser.close();
 })();
