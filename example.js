5194f757fea1a509007dbcc8c80403f791e1b29e
Dark Tron <darktron1337@gmail.com>
2021-04-28 15:06:59 +0200

    recording yt works, but only with head :(

--- example.js
+++ example.js
@@ -1,28 +1,33 @@
 const puppeteer = require('puppeteer');
-const PuppeteerVideoRecorder = require('puppeteer-video-recorder');
+const { launch, getStream } = require("puppeteer-stream");
+const fs = require("fs");
 
 const BBB_ROOM = 'sus-c9t-jhz';
-const recorder = new PuppeteerVideoRecorder();
+const filename = `./recordings/hci_${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}.webm`;
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
+  const browser = await launch({
+    headless: true, // aaaarrrwwww why!?!?!?!?!?
     args: [
       '--lang=en-EN,en',
+      "--window-size=1280,720"
     ],
     ignoreDefaultArgs: [
       '--mute-audio'
     ],
-    defaultViewport: {width: 1280, height: 720}
+    defaultViewport: null,
   });
 
   const page = await browser.newPage();
-  await recorder.init(page, 'recordings');
 
+  //await page.goto('https://www.youtube.com/watch?v=BRVSF_OqcTI');
+  
   // go to bbb
+  console.log('going to bbb');
   await page.goto('https://webconf.tu-bs.de/' + BBB_ROOM);
 
   // login
+  console.log('logging in');
   await (await page.$('#_' + BBB_ROOM + '_join_name')).type('Nagrywarka');
   await (await page.$('button[type=submit]')).click();
 
@@ -31,26 +36,35 @@ const recorder = new PuppeteerVideoRecorder();
     waitUntil: 'networkidle0',
   });
   await page.waitForSelector('input[type=submit]', {
-      timeout: 1000 * 60 * 60 // one hour
+      timeout: 1000 * 60 * 60 * 2 // two hours
   });
+  console.log('accepting cookies');
   await (await page.$('input[type=submit]')).click();
 
   // join audio session
   await page.waitForNavigation({
     waitUntil: 'networkidle0',
-  });  
+  });
   await page.waitForSelector('button[aria-label="Listen only"]');
+  console.log('joining audio session');
   await (await page.$('button[aria-label="Listen only"]')).click();
 
   // start recording
-  await recorder.start({
-      quality: 70
-  });
-  //page.waitForTimeout(5000);
-  const input = await page.$('#message-input');
-  await input.type('in english i guess', { delay: 200 });
+  console.log('starting recording');
+  const file = fs.createWriteStream(filename);
 
-  // end recording
-  await recorder.stop();
-  await browser.close();
+  const stream = await getStream(page, {
+    audio: true,
+    video: true,
+    audioBitsPerSecond: 64 * 1024,
+    videoBitsPerSecond: 1 * 1024 * 1024
+  });
+
+  stream.pipe(file);
+
+  setTimeout(async () => {
+    await stream.destroy();
+    file.close();
+    await browser.close();
+  }, 1000 * 60 * 60 * 2); // two hours
 })();
